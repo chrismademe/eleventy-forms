4e14fe1ca1ebdbb9b5806d7431d9ecdf297ecb9d
Chris Galbraith <chris@resknow.co.uk>
2021-03-16 16:51:21 +0000

    Setup for NPM

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+const forms = require('./lib/loadConfig')();
+const Form = require('./lib/classes/Form');
+
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addShortcode('renderForm', (name) => {
+
+        // Bail if the form doesn't exist
+        if ( ! _.has(forms, name) ) {
+            throw new Error(`No form was found with the name ${name}, check your eleventyForms.js config file.`);
+        }
+
+        let theForm = new Form(form);
+        return theForm.render();
+    });
+}
